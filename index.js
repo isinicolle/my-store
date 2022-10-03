2f3b88ae7fe827ab578734b7bdcdd6ac01129652
isisflorentino <isis.zapata.hn@gmail.com>
2022-10-02 22:40:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) =>{
   res.send('Hello World!')
 });
 
+
+app.get('/users', (req, res) => {
+  res.json([
+    {id: 1, name: 'John Doe'},
+    {id: 2, name: 'Jane Doe'}
+  ]);
+});
+
+app.get('/users/:id', (req, res) => {
+  res.json({id: 1, name: 'John Doe'});
+});
+
 app.listen(port , () => {
   console.log('Mi port', port)
 })
