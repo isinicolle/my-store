58b9096600f8e0279f8f7058010a5dfca5adbe8a
isisflorentino <isis.zapata.hn@gmail.com>
2022-10-02 23:38:55 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const productRoutes = require('./productRoutes');
 
 function routes(app) {
+  //ruta maestra
+const router = express.Router();
+  //ruta hija
+  app.use('/api/v1', router);
+
   app.use('/api/products', productRoutes);
 }
 
