74755ae62075a8f1c2846d98b7d0361058dff6ac
isisflorentino <isis.zapata.hn@gmail.com>
2022-10-02 23:43:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,18 @@
 const express = require('express');
-const faker = require('faker');
+const  routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
 
+//middleware
+app.use(express.json());
+
+//routes
 app.get('/', (req, res) =>{
   res.send('Hello World!')
 });
 
-
-app.get('/users', (req, res) => {
-  res.json([
-    {id: 1, name: 'John Doe'},
-    {id: 2, name: 'Jane Doe'},
-    {id: 3, name: 'John Smith'},
-    {id: 4, name: 'Jane Smith'}
-  ]);
-});
-
-app.get('/users/filtrer', (req, res) => {
-  console.log('Lo especifico va primero que lo dinamico')
-});
-
-app.get('/users/:Userid', (req, res) => {
-  const {Userid} = req.params.Userid;
-  res.json({id: Userid, name: 'John Doe'});
-});
-
-app.get('/users/:Userid/member/:memberId', (req, res) => {
-      const {Userid, memberId} = req.params;
-      res.json({
-        Userid: Userid,
-        memberId: memberId
-      });
-
-
-});
-
-app.get('/productos', (req, res) => {
-  const {limit, offset} = req.query;
-
-  if(limit && offset){
-    res.json({
-      limit: limit,
-      offset: offset
-    });
-  } else {
-    res.send('No params');
-  }
-});
-
-app.get('/products', (req, res) => {
-const products = [];
-const {size} = req.query;
-const limit = size || 10;
-for(let i = 0; i < limit; i++){
-  products.push({
-    id: faker.random.uuid(),
-    name: faker.commerce.productName(),
-    price: parseInt(faker.commerce.price()),
-    image: faker.image.imageUrl()
-  });
-}
-res.json(products);
-
-
-});
-
-
+routerApi(app);
 
 app.listen(port , () => {
   console.log('Mi port', port)
