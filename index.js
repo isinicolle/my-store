a5478f0cc56213acff4e49e45db2037f7ddc562e
isisflorentino <isis.zapata.hn@gmail.com>
2022-10-02 22:51:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,25 @@ app.get('/', (req, res) =>{
 app.get('/users', (req, res) => {
   res.json([
     {id: 1, name: 'John Doe'},
-    {id: 2, name: 'Jane Doe'}
+    {id: 2, name: 'Jane Doe'},
+    {id: 3, name: 'John Smith'},
+    {id: 4, name: 'Jane Smith'}
   ]);
 });
 
-app.get('/users/:id', (req, res) => {
-  res.json({id: 1, name: 'John Doe'});
+app.get('/users/:Userid', (req, res) => {
+  const {Userid} = req.params.Userid;
+  res.json({id: Userid, name: 'John Doe'});
+});
+
+app.get('/users/:Userid/member/:memberId', (req, res) => {
+      const {Userid, memberId} = req.params;
+      res.json({
+        Userid: Userid,
+        memberId: memberId
+      });
+
+
 });
 
 app.listen(port , () => {
